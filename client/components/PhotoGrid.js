aee845507478968d341daccd1c0395c98ae02027
willsputra <suryadiputrawilliam@gmail.com>
2017-09-26 09:04:48 +0800

    first

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+    render(){
+        return(
+            <div>
+                <h1>
+                    <div className="photo-grid">
+                        I'm a PhotoGrid
+                    </div>
+                </h1>
+            </div>
+        )
+    }
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Single = React.createClass({
+    render(){
+        return(
+            <div>
+                <h1>
+                    <div className="single-photo">
+                        I'm a Single
+                    </div>
+                </h1>
+            </div>
+        )
+    }
+});
+
+export default Single;
